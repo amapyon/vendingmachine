7c12e6721b6ffc795c90202f6d56663068943260
amapyon <github@amapyon.org>
2014-10-05 19:10:47 +0900

    テストパス。

--- vendingmachine/src/tddbc/vm/VendingMachine.java
+++ vendingmachine/src/tddbc/vm/VendingMachine.java
@@ -2,12 +2,18 @@ package tddbc.vm;
 
 public class VendingMachine {
 
+	private int amount;
+
+	public VendingMachine() {
+		amount = 0;
+	}
+
 	public int amount() {
-		return 0;
+		return amount;
 	}
 
 	public void pay(int amount) {
-
+		this.amount += amount;
 	}
 
 }
