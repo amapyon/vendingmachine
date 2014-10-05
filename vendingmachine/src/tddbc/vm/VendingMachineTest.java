937fb536ea5e6fdb56650bf3e94b726fbee94243
amapyon <github@amapyon.org>
2014-10-05 19:17:01 +0900

    ローカル変数をインスタンス変数に変更。 setpUp()でインスタンス生成するように変更。

--- vendingmachine/src/tddbc/vm/VendingMachineTest.java
+++ vendingmachine/src/tddbc/vm/VendingMachineTest.java
@@ -2,20 +2,29 @@ package tddbc.vm;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VendingMachineTest {
 
+	private VendingMachine vm;
+
+	@Before
+	public void setUp() {
+		vm = new VendingMachine();
+	}
+
 	@Test
 	public void testAmountWhenInitialized() {
-		VendingMachine vm = new VendingMachine();
 		assertEquals(0, vm.amount());
 	}
 
 	@Test
 	public void testPay100() throws Exception {
-		VendingMachine vm = new VendingMachine();
 		vm.pay(100);
 		assertEquals(100, vm.amount());
+	}
+
+	public void testPays() throws Exception {
 	}
 }
